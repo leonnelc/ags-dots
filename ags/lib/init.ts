9dfb415530dbf484563babfcb4e2dd8e94c18ffa
leonnelc <leonelcortes2424@gmail.com>
2025-08-16 19:04:45 -0300

    remove support for tmux & hyprland config

--- ags/lib/init.ts
+++ ags/lib/init.ts
@@ -1,18 +1,14 @@
 import matugen from "./matugen"
-import hyprland from "./hyprland"
-import tmux from "./tmux"
 import gtk from "./gtk"
-import lowBattery from "./battery"
+import batteryWatcher from "./battery"
 import notifications from "./notifications"
 
 export default function init() {
     try {
         gtk()
-        tmux()
         matugen()
-        lowBattery()
+        batteryWatcher()
         notifications()
-        hyprland()
     } catch (error) {
         logError(error)
     }
