9a1c707ec5855ee10616c089fcc3a65c73b80624
leonnelc <leonelcortes2424@gmail.com>
2025-08-16 19:16:58 -0300

    feat(quicksettings widget): add file exists check for avatar image & add tooltips

--- ags/widget/quicksettings/widgets/Header.ts
+++ ags/widget/quicksettings/widgets/Header.ts
@@ -2,18 +2,23 @@ import icons from "lib/icons"
 import { uptime } from "lib/variables"
 import options from "options"
 import powermenu, { Action } from "service/powermenu"
+import GLib from "gi://GLib?version=2.0"
 
 const battery = await Service.import("battery")
 const { image, size } = options.quicksettings.avatar
-
+if (image) image.value = GLib.file_test(image.value, GLib.FileTest.EXISTS) ? image.value : "";
 function up(up: number) {
     const h = Math.floor(up / 60)
     const m = Math.floor(up % 60)
+    const d = Math.floor(up /60/60)
+    if (h >= 24) 
+      return `${d}d ${h%24 < 10 ? "0" + h%24 : h%24}h`;
     return `${h}h ${m < 10 ? "0" + m : m}m`
 }
 
 const Avatar = () => Widget.Box({
     class_name: "avatar",
+    visible: image.bind().as( img => !!img),
     css: Utils.merge([image.bind(), size.bind()], (img, size) => `
         min-width: ${size}px;
         min-height: ${size}px;
@@ -39,12 +44,12 @@ export const Header = () => Widget.Box(
                 visible: battery.bind("available"),
                 children: [
                     Widget.Icon({ icon: battery.bind("icon_name") }),
-                    Widget.Label({ label: battery.bind("percent").as(p => `${p}%`) }),
+                    Widget.Label({ label: battery.bind("percent").as(p => `${p}%`), tooltipText: "Battery"}),
                 ],
             }),
             Widget.Box([
                 Widget.Icon({ icon: icons.ui.time }),
-                Widget.Label({ label: uptime.bind().as(up) }),
+                Widget.Label({ label: uptime.bind().as(up), tooltipText: "Uptime"}),
             ]),
         ],
     }),
